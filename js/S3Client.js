eae6e03b27bda64c1326f045f1adde759095fcac
eduardfores <eduardfores@gmail.com>
2022-03-05 12:41:06 +0100

    Main: fix credentials git relation and separate S3 funcitons

--- js/S3Client.js
+++ js/S3Client.js
@@ -15,14 +15,4 @@ S3ClientFactory.newClient = function (config) {
   
     return s3;
 };
-
-// List the photo albums that exist in the bucket.
-function listAlbums() {
-    var s3 = S3ClientFactory.newClient();
-
-    var result = s3.listObjects({ Delimiter: '/posts' }, function (err, data) {
-        if (err) console.log(err, err.stack);
-        else console.log(data);
-    });
-}
   
--- js/credentials/credentialsS3.js
+++
@@ -1,6 +0,0 @@
-//Fill the constants with your infromation form AWS
-const BUCKET_NAME = '';
-
-const REGION_NAME = '';
-
-const IDENTITY_POOL_ID = '';
